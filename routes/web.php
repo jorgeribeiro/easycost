4d0b3f50a22ada4ebc4b37adc5d70bd4fa81ac4c
Jorge Ribeiro <joorgemelo@gmail.com>
2018-12-18 08:58:24 -0300

    Inserindo novas páginas

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,22 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 });
 
+Route::get('/orcamentos', function () {
+    return view('orcamentos');
+});
+
+Route::get('/composicoes', function () {
+    return view('composicoes');
+});
+
+Route::get('/insumos', function () {
+    return view('insumos');
+});
+
+Route::get('/relatorios', function () {
+    return view('relatorios');
+});
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
